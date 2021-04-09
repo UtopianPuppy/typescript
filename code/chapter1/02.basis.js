ceb859f1597a528565d2c9c6ed70d642353a1c57
utopianpuppy <734507502@qq.com>
2021-04-09 17:10:13 +0800

    learn

---
+++ code/chapter1/02.basis.js
@@ -0,0 +1,7 @@
+// 声明一个变量a，同时指定它的类型为number
+var a;
+a = 10;
+a = 33;
+// a = 'ddd'
+var b;
+b = 'aaa';
---
+++ code/chapter1/02.basis.ts
@@ -0,0 +1,9 @@
+// 声明一个变量a，同时指定它的类型为number
+let a: number
+
+a = 10;
+a = 33;
+// a = 'ddd'
+
+let b: string;
+b = 'aaa'
